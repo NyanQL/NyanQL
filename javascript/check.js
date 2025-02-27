6b7fb41fcf64d115e5ecd0d5991ddf4495a0b936
Takako Miyagawa <nekoget@gmail.com>
2025-02-27 20:49:31 +0900

    configの読み込みとcheckの実行

---
+++ javascript/check.js
@@ -0,0 +1,7 @@
+function main() {
+    if (!nyanAllParams.token) {
+        return { status: 500, success: false, error: {"test": "hogehoge" , "2": "エラー2つめ"} };
+    }
+    return { status: 200, success: true };
+}
+main();
