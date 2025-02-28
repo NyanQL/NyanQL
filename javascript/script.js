fc1c51d48a663b7dba3a7b13445a0aa1d097fb0b
Takako Miyagawa <nekoget@gmail.com>
2025-03-01 07:54:53 +0900

    javascriptファイルを更新

--- javascript/script.js
+++ javascript/script.js
@@ -3,21 +3,20 @@ const nyanAcceptedParams = {"date": "2024-06-25"};
 
 const nyanOutputColumns =  ["date"];
 
-
+console.log("loaded script.js");
 
 
 function main(){
     console.log(nyanAllParams);
-    let err = nyanRequestCheck(Object.keys(nyanAcceptedParams) , nyanAcceptedParams);
-    console.log("check", err);
-    let result = JSON.parse(nyanRunSQL("./sql/sqlite/check_day.sql" , {"date": "2024%"}));
-    if(result[0].this_days_count == 1)
-    {
-        console.log(1);
-    }
+    let data = nyanAllParams;
+    console.log(JSON.stringify(data));
+    data.ids = [2];
+    console.log(data);
+    let result = JSON.parse(nyanRunSQL("./sql/sqlite/get_by_ids.sql" , data));
+
     console.log(JSON.stringify(result));
-    console.log(JSON.stringify(nyanErros));
-    return JSON.stringify({success: true, status: 200, data: { result: result, request: nyanAllParams} });
+
+    return JSON.stringify({success: true, status: 200, data: { result: result} });
 }
 
 main();
