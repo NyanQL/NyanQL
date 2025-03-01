47b9ff9295e1db297df2055303d74a28bb8e19e5
Takako Miyagawa <nekoget@gmail.com>
2025-03-01 17:21:13 +0900

    SQL修正

--- sql/sqlite/get_by_ids.sql
+++ sql/sqlite/get_by_ids.sql
@@ -1,8 +1,8 @@
 SELECT id, date FROM stamps
 /*BEGIN*/
-/*IF BEGIN */ WHERE /*END*/
+ WHERE
     /*IF id != null*/ id = /*id*/1 /*END*/
-    /*IF BEGIN AND id != null*/ AND /*END*/
+    /*IF id != null AND date != null */ AND /*END*/
     /*IF date != null*/ date = /*date*/"2024-06-25" /*END*/
 /*END*/
 ;
