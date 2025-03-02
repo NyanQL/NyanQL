c21e0863c590adda5721b8978d68097b8876cc76
Takako Miyagawa <nekoget@gmail.com>
2025-03-02 20:17:18 +0900

    /nyanの修正とsqlの修正など

--- sql/sqlite/get_by_ids.sql
+++ sql/sqlite/get_by_ids.sql
@@ -1,8 +1,1 @@
-SELECT id, date FROM stamps
-/*BEGIN*/
- WHERE
-    /*IF id != null*/ id = /*id*/1 /*END*/
-    /*IF id != null AND date != null */ AND /*END*/
-    /*IF date != null*/ date = /*date*/"2024-06-25" /*END*/
-/*END*/
-;
+SELECT id, date FROM stamps where id in (/*ids*/1);
